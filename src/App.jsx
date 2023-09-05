ca2cfb85dc3d1579ca1397fe5e5b73b45f356027
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2023-09-05 09:00:24 -0700

    adding pictures

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
+
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-       Hello world!
-      </h1>
+    <div className='w-screen h-auto flex flex-col'>
+     <Header/>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const Header = () => {
+  return (
+    <div className='bg-yellow-400'>Header</div>
+  )
+}
+
+export default Header
